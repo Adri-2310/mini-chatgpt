6784baf88b7a7ab7f8a1bd0e77daf2db6dfe570e
Adrien Mertens <adrien231020@gmail.com>
2026-06-14 12:10:53 +0200

    Corriger le chemin d'import du composant Checkbox

--- resources/js/Components/Checkbox.vue
+++ resources/js/Components/Checkbox.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { computed } from 'vue';
-import { Checkbox } from '@/components/ui/ui/checkbox'
+import { Checkbox } from '@/components/ui/checkbox'
 
 const props = defineProps({
     checked: {
